a6614b8951e8906340fdab3dd2719cd1d997f3a5
flashwebdeveloper <wasim.kabir92@gmail.com>
2017-11-16 07:56:31 +0530

    Forms, events and inserting into the collection (with sorting)

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,26 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task' (event) {
+    // Prevent default browser from submit
+    event.preventDefault();
+
+    // Get the value from form input
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
   },
 });
